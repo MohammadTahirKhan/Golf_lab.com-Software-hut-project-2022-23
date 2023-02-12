71043ae4d624c5c3a694f01aa41bb1b70ebbb8b5
Ethan Watts <eawatts1@sheffield.ac.uk>
2023-02-12 16:21:49 +0000

    Sentry setup

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   # Sentry is only enabled when the dsn is set.
   unless Rails.env.development? || Rails.env.test?
-    config.dsn = 'REPLACE_WITH_YOUR_DSN'
+    config.dsn = 'https://b2b39a8d87874576ae56ab52f7f73307@sentry.shefcompsci.org.uk/355'
   end
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
   config.before_send = -> (event, hint) { ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters).filter(event.to_hash) }
