2e5b9b9652bbfeac404fb28b2074c75b99014383
Ethan Watts <eawatts1@sheffield.ac.uk>
2023-03-06 20:42:35 +0000

    custom shape types added

--- app/packs/scripts/maptest.js
+++ app/packs/scripts/maptest.js
@@ -1,89 +1,100 @@
-import L, { LatLng, latLng, LatLngUtil, Point, Projection, SVG } from "leaflet";
-import LD from "leaflet-draw";
+import L from "leaflet";
+import "leaflet-draw";
 
-var davenportGolfClub = new L.LatLng(53.3513668,-2.0975508),
-    map = L.map('map', {center: davenportGolfClub, zoom: 16}),
-    satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    }),
-    drawnItems = new L.featureGroup();
+
+const davenportGolfClub = new L.LatLng(53.3513668,-2.0975508);
+const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+
+var map = L.map('map', {center: davenportGolfClub, zoom: 16}),
+    map2 = L.map('map2', {center: davenportGolfClub, zoom: 16, minZoom: 14, maxZoom: 18});
 
 satelliteLayer.addTo(map);
-map.addLayer(drawnItems);
+L.control.scale({position: 'topright'}).addTo(map);
+L.control.scale({position: 'topright'}).addTo(map2);
 
-var drawOptions = {
+var fairways = new L.FeatureGroup(),
+    greens = new L.FeatureGroup(),
+    roughs = new L.FeatureGroup(), 
+    hazards = new L.FeatureGroup(), 
+    tees = new L.FeatureGroup();
+
+var options = {
     position: 'topleft',
     draw: {
-        polygon: true,
         polyline: false,
+        polygon: {
+            allowIntersection: false,
+        },
         circle: false,
         rectangle: false,
         marker: false,
         circlemarker: false
     },
     edit: {
-        featureGroup: drawnItems,
+        featureGroup: fairways,
         remove: true
     }
 };
 
-var drawControl = new L.Control.Draw(drawOptions);
-map.addControl(drawControl);
+var drawControl = new L.Control.Draw(options);
+function initialiseDrawControl(type, colour) {
+    options.edit.featureGroup = type;
+    options.draw.polygon.shapeOptions = {color: colour};
+    map.removeControl(drawControl);
+    
+    drawControl = new L.Control.Draw(options);
+    map.addControl(drawControl);
+}
+
+
+var shapeType = document.getElementById('shape-type');
+shapeType.onchange = function() {
+    switch (shapeType.value) {
+        case 'fairway':
+            initialiseDrawControl(fairways, 'rgb(190, 255, 190)');
+            break;
+        case 'green':
+            initialiseDrawControl(greens, 'rgb(0, 255, 0)');
+            break;
+        case 'rough':
+            initialiseDrawControl(roughs, 'rgb(100, 255, 100)');
+            break;
+        case 'hazard':
+            initialiseDrawControl(hazards, 'rgb(255, 0, 0)');
+            break;
+        case 'tee':
+            initialiseDrawControl(tees, 'rgb(255, 255, 255)');
+    }
+}
 
 
 map.on('draw:created', function (e) {
-    var type = e.layerType,
-        layer = e.layer,
-        para = document.getElementById("coords"),
-        coords = layer.getLatLngs()[0];
+    var layer = e.layer;
 
-    para.innerHTML = coords;
+    switch (shapeType.value) {
+        case 'fairway':
+            fairways.addLayer(layer);
+            break;
+        case 'green':
+            greens.addLayer(layer);
+            break;
+        case 'rough':
+            roughs.addLayer(layer);
+            break;
+        case 'hazard':
+            hazards.addLayer(layer);
+            break;
+        case 'tee':
+            tees.addLayer(layer);
+    }
 
-    var shapePoints = [];
-    coords.forEach(function(coord) {
-        var point = map.latLngToLayerPoint(coord);
-        shapePoints.push(point);
-    });
-
-    var svg = document.getElementById("svg");
-    var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    svg.appendChild(polygon);
-
-    shapePoints.forEach(function(value) {
-        var point = svg.createSVGPoint();
-        point.x = value.x;
-        point.y = value.y;
-        polygon.points.appendItem(point);
-      }
-    );
-    
-    drawnItems.addLayer(layer);
 });
 
-// satelliteLayer.addTo(map);
+map.addLayer(fairways);
+map.addLayer(greens);
+map.addLayer(roughs);
+map.addLayer(hazards);
+map.addLayer(tees);
 
-// L.control.layers(
-//     {'Satellite': satelliteLayer.addTo(map)},
-//     {'drawLayer': drawnItems},
-//     {position: 'topleft', collapsed: false}
-// ).addTo(map);
-
-// map.addControl(new L.Control.Draw({
-//     edit: {
-//         featureGroup: drawnItems,
-//         poly: {
-//             allowIntersection: false
-//         }
-//     },
-//     draw: {
-//         polygon: {
-//             allowIntersection: false,
-//             showArea: true
-//         }
-//     }
-// }));
-
-// map.on(L.Draw.Event.CREATED, function(e) {
-//     var layer = e.layer;
-//     drawnItems.addLayer(layer);
-// });
+initialiseDrawControl(fairways, 'rgb(190, 255, 190)');
