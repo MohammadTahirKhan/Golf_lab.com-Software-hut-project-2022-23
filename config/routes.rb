dcf651bf42b30e79440d2e0a303dbaceb47f7ef9
Connor Malcolm <cmalcolm1@sheffield.ac.uk>
2023-03-21 17:39:03 +0000

    profile fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get 'users/sign_out' => "devise/sessions#destroy"
+    patch 'users/edit' => "devise/registrations#edit"
   end
 
   # get '/holes/:id/edit', to: 'data#index'
