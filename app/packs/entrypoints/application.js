586586e1812bc85db5afdc44b76a828895a8227e
Ethan Watts <eawatts1@sheffield.ac.uk>
2023-04-10 11:35:35 +0100

    flash msgs fixed on login

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,4 +1,7 @@
 import Rails from "@rails/ujs";
 import "bootstrap";
+import removeFlashMsg from "../scripts/layout";
+
+window.removeFlashMsg = removeFlashMsg;
 
 Rails.start();
---
+++ app/packs/scripts/layout.js
@@ -0,0 +1,6 @@
+const removeFlashMsg = () => {
+    const flashMsg = document.querySelector('.alert');
+    flashMsg.remove();
+};
+
+export default removeFlashMsg;
