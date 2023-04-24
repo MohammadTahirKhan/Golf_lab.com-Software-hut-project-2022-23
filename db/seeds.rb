169abc59460345887ec60d3be925ec6af75bd7db
Mohammad Tahir Khan <mtkhan1@sheffield.ac.uk>
2023-04-24 14:55:32 +0100

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 # Course.create(name:"UsmanHCourse")
 # Hole.create(hole_number:8, is_beingEdited:false, course_id:1, user_id:1)
-
 Hole.create(hole_number:18, is_beingEdited:false, user_id:2, course_name: "TestCourse")
 UserHole.create(hole_number:18, user_id:2, hole_id:2)
+User.create(id:1111, email:"admin@sheffield.ac.uk", password:"123456", user_role:"admin")
+User.create(id:2222, email:"mapcreator@sheffield.ac.uk", password:"123456", user_role:"map_creator")
