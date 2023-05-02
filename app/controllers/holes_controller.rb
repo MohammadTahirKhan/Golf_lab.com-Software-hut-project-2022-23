708e0a19accea75f626d04c752378f5259512720
Mohammad Tahir Khan <mtkhan1@sheffield.ac.uk>
2023-05-02 18:11:38 +0100

    added comments

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -2,6 +2,7 @@ class HolesController < ApplicationController
   before_action :set_hole, only: %i[ show edit update destroy ]
   before_action :require_map_creator
 
+  # Require map creator role to access holes page
   def require_map_creator
     unless current_user.user_role == "map_creator" || current_user.user_role == "admin"
       redirect_to root_path
